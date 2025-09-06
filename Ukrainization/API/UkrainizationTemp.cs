9199399f129027b3ffdbc4f4cccbd62a518cbc84
DenysCrasav4ik <147797215+Denyscrasav4ik@users.noreply.github.com>
2025-09-06 14:25:55 +0300

    Update UkrainizationTemp.cs

--- Ukrainization/API/UkrainizationTemp.cs
+++ Ukrainization/API/UkrainizationTemp.cs
@@ -8,7 +8,7 @@ namespace Ukrainization.API
     {
         public const string ModGUID = "Ukrainization";
         public const string ModName = "Baldi's Basics Plus Ukrainization";
-        public const string ModVersion = "1.0.0.4";
+        public const string ModVersion = "1.0.0.5";
 
         public static string PostersFile = "PosterSettings.json";
     }
